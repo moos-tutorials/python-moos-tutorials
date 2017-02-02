acc8c0c5c03f43e1301500d12d99d3cd3bad7e3a
Mohamed Saad Ibn Seddik <ms.ibnseddik@gmail.com>
2017-02-02 01:32:08 -0500

    [00] adds simple-comms

---
+++ 00-simple-comms/README.md
@@ -0,0 +1,10 @@
+# Ping-Pong simple example
+
+This is a very basic example where the `ping.py` app sends a variable **PING**
+with the value **Hello world!** and the `pong.py` app responds by the variable
+**PONG** with the number of **PING** s received.
+
+## Usage
+```shell
+pAntler simple_pingpong.moos
+```
---
+++ 00-simple-comms/ping.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+from pymoos import pymoos
+import time
+
+class pingMOOS(pymoos.comms):
+    """pingMOOS is an example python MOOS app.
+    It basically just connects to the MOOSDB
+
+    Attributes:
+        moos_community: a string representing the address of the Community
+        moos_port:      an interger defining the port
+    """
+
+    def __init__(self, moos_community, moos_port):
+        """Initiates MOOSComms, sets the callbacks and runs the loop"""
+        super(pingMOOS, self).__init__()
+        self.server = moos_community
+        self.port = moos_port
+        self.name = 'pingMOOS'
+
+        self.set_on_connect_callback(self.__on_connect)
+        self.run(self.server, self.port, self.name)
+
+    def __on_connect(self):
+        """OnConnect callback"""
+        print("Connected to", self.server, self.port,
+              "under the name ", self.name)
+        return True
+
+
+
+def main():
+    pinger = pingMOOS('localhost', 9001)
+
+    while True:
+        time.sleep(1)
+        pinger.notify('PING', 'Hello world!', -1);
+
+if __name__ == "__main__":
+    main()
